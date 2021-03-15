e912e97be7c61882f316b72ed2af38b96cdd0fc2
toi0214 <78341100+toi0214@users.noreply.github.com>
2021-03-15 15:13:11 +0900

    商品詳細機能 1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
-  
+  before_action :authenticate_user!, only: [:new, :create, :show]
+  before_action :set_item, only: [:show]
   
   def index
     @items = Item.all.order("created_at DESC")
@@ -19,12 +19,21 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @user = Item.new
+    @items = @item.user
+  end
 
+# showアクションを定義し、特定の商品情報w出力する @itemをその中で定義する
 
   private
 
   def item_params
     params.require(:item).permit(:name, :info, :category_id, :image, :status_id, :area_id, :delivery_fee_id, :delivery_day_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
 end
