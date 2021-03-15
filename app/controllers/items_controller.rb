664cbf4e7f9de44e1aaeae90c9e28333d4293031
toi0214 <78341100+toi0214@users.noreply.github.com>
2021-03-15 15:52:09 +0900

    商品詳細機能 2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :show]
+  before_action :authenticate_user!, only: [:new, :create]
   before_action :set_item, only: [:show]
   
   def index
