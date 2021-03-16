c160bb7f4a33364949ced178631ceabc079ca129
toi0214 <78341100+toi0214@users.noreply.github.com>
2021-03-16 11:58:53 +0900

    商品削除機能 2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
-  before_action :item_info, only: [:update, :edit]
+  before_action :item_info, only: [:update, :edit, :destroy]
   
   def index
     @items = Item.all.order("created_at DESC")
