b91e17dbb54a767956ee67f222234a25808b4199
toi0214 <78341100+toi0214@users.noreply.github.com>
2021-03-16 10:32:49 +0900

    商品情報編集機能 3

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,12 +24,10 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @item = Item.find(params[:id])
     redirect_to root_path if current_user.id != @item.user_id
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
