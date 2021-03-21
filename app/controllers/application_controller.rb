ec8933576cae2ca822f7fdccbf1f6f9506d99396
toi0214 <78341100+toi0214@users.noreply.github.com>
2021-03-21 15:53:04 +0900

    S3 導入 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
   private
 
@@ -6,5 +7,11 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(
       :sign_up, keys: [:nickname, :email, :first_name, :second_name, :given_name, :last_name, :birthday_id]
     )
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
   end
 end
